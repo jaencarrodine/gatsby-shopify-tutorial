ee3c5b838273922ab0a324ed4ab0b0269fcf34a9
Jaen <jaencarrodine@gmail.com>
2022-04-11 18:38:54 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ require("dotenv").config()
 
 module.exports = {
   siteMetadata: {
-    title: `Shopify Tutorial`,
-    description: `Create a Gatsby site connected to your Shopify store`,
-    author: `@stephdiep`,
-    siteUrl: `https://designcode.io`,
+    title: `Bender`,
+    description: `Electrolyte powder formulated to prevent hangovers.`,
+    author: ``,
+    siteUrl: `https://bender.buzz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,14 +28,37 @@ module.exports = {
         start_url: `/`,
         background_color: `#014C40`,
         display: `minimal-ui`,
-        icon: `src/images/coffee-cup.svg`, // This path is relative to the root of the site.
+        icon: `src/images/BenderB-White.png`, // This path is relative to the root of the site.
       },
     },
     {
       resolve: `gatsby-source-shopify`,
       options: {
-        password: process.env.GATSBY_SHOPIFY_PASSWORD,
+        password: process.env.SHOPIFY_SHOP_PASSWORD,
         storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-TB0CVLSRJC', // Google Analytics / GA
+          'AW-CONVERSION_ID', // Google Ads / Adwords / AW
+          'DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
       },
     },
   ],
